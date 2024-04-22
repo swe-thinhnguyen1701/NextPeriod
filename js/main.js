f53f4232530a366c10468c19c5993ce23f19d7a1
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-04-21 22:21:49 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const calculate = document.getElementById("calculation");
+const day = document.getElementById("dd");
+const month = document.getElementById("mm");
+const year = document.getElementById("yyyy");
+const MONTH = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const DAY = ["Monday", "Tuseday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
